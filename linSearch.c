b590eb98d630629fd647924aadf21e31ef097e94
Abhinav <abhinavdhere@gmail.com>
2015-09-13 10:56:23 +0530

    Linear Search Algo

--- linSearch.c
+++ linSearch.c
@@ -2,16 +2,34 @@
 
 #include <stdio.h>
 
+//function declarations
 int lsIter(int*,int,int);
+int lsRecur(int*,int,int);
 
-int lsIter(int* list, int res, int n)
+//function definitions
+int lsRecur(int* list,int res, int n) //recursive linear search
 {
-int i,flag=0;
+static int i=1;                       //static variable is not lost after control goes out of function
+//printf("%p %d %d\n",list,*list,res); //for debugging, shows the flow of the function
+if (res==(*list))                       //match found
+    return 1;
+if (i>n)                                //indicates end of array
+    return 0;
+else
+    {
+    i++;                                //increment count for index of array
+    return lsRecur(list+1,res,n);       //call function for next value in array
+    }
+}
+
+int lsIter(int* list, int res, int n)   //iterative linear search
+{
+int i,flag=0;                           //flag indicates whether term found. 0 if not found.
 for (i=0;i<n;i++)
     {
     if (list[i]==res)
         {        
-        flag=1;
+        flag=1;                         //flag=1 indicates that term found
         break;
         }
     }
@@ -28,8 +46,15 @@ for (i=0;i<n;i++)
     scanf("%d",&list[i]);
 printf("What would you like to search?\n");
 scanf("%d",&res);
-if (lsIter(&list[0],res,n))
-    printf("Number is in list\n");
+
+if (lsIter(&list[0],res,n))             //call iterative search
+printf("Iterative: Number is in list\n");
 else
-    printf("Number is not in list\n");
+printf("Iterative: Number is not in list\n");
+
+if (lsRecur(&list[0],res,n))            //call recursive search
+    printf("Recursive: Number is in list\n");
+else
+    printf("Recursive: Number is not in list\n");
 }
+
